45a60cd975da83d337b95e3b0f17cb1edb179620
icekunx <icekunxp@gmail.com>
2021-02-13 14:23:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 require_once __DIR__ . '/lineBot.php';
-
 $bot = new Linebot();
-if $bot->getMessageText() {
-$text = "ทดสอบ";
-}
-$bot->reply($text);
+
+$text = $bot->getMessageText(); //when user send text to bot	
+$bot->reply($text); // bot reply to user
+?>
 
 
 
